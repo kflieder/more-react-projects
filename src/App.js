785fc77fc57cc661e267efaa8be3f334090b208a
kflieder <kacey.flieder@gmail.com>
2024-03-10 12:36:09 -0600

    image slider
    finished the image slider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Accordian from './components/accordian';
+import ImageSlider from './components/image-slider';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 
@@ -9,6 +10,7 @@ function App() {
     <Accordian />
     <RandomColor />
     <StarRating noOfStars={10} />
+    <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/>
     </>
   );
 }
