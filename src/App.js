6d97ee3943f327f475e17d5dbcc9308bd5a4dac2
kflieder <kacey.flieder@gmail.com>
2024-03-10 20:50:37 -0600

    TreeView
    added an expandable tree view menu

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import ImageSlider from './components/image-slider';
 import LoadMoreData from './components/load-more-data';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import TreeView from './components/tree-view';
+import menus from './components/tree-view/data';
 
 function App() {
   return (
     <>
-    <Accordian />
+    {/* <Accordian />
     <RandomColor />
     <StarRating noOfStars={10} />
     <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/>
-    <LoadMoreData />
+    <LoadMoreData /> */}
+    <TreeView menus={menus} />
     </>
   );
 }
