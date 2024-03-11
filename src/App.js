7a6b596d88e1e6b12dd881bb68466742142d3e95
kflieder <kacey.flieder@gmail.com>
2024-03-11 16:17:28 -0600

    QR Generator
    created a QR generator

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Accordian from './components/accordian';
 import ImageSlider from './components/image-slider';
 import LoadMoreData from './components/load-more-data';
+import QRCodeGenerator from './components/qr-code-generator';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import TreeView from './components/tree-view';
@@ -10,12 +11,13 @@ import menus from './components/tree-view/data';
 function App() {
   return (
     <>
-    {/* <Accordian />
+    <Accordian />
     <RandomColor />
     <StarRating noOfStars={10} />
     <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/>
-    <LoadMoreData /> */}
+    <LoadMoreData />
     <TreeView menus={menus} />
+    <QRCodeGenerator />
     </>
   );
 }
---
+++ src/components/qr-code-generator/index.jsx
@@ -0,0 +1,26 @@
+import QRCode from "react-qr-code"
+import { useState } from "react"
+
+export default function QRCodeGenerator() {
+
+    const [qrCode, setQrCode] = useState('');
+    const [input, setInput] = useState('');
+
+    function handleGenerateQrCode() {
+        setQrCode(input);
+        setInput('');
+    }
+
+    return (
+        <div>
+            <h1>QR Code Generator</h1>
+            <div>
+                <input onChange={(e) => setInput(e.target.value)} type="text" name="qr-code" placeholder="Enter your value here" value={input} />
+                <button disabled={input && input.trim() !== "" ? false : true} onClick={handleGenerateQrCode}>Generate</button>
+            </div>
+            <div>
+                <QRCode id="qr-code-value" value={qrCode} size={400} bgColor="#fff" />
+            </div>
+        </div>
+    )
+}
